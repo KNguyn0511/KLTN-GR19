237b8699ded434ddfe61167e0470efcf023af75d
huynguyen08 <huyhiquama@gmail.com>
2026-04-05 12:27:10 +0700

    chore: cleanup misstake folder in root folder and move to frontend folder

--- features/products/components/index.tsx
+++
@@ -1,7 +0,0 @@
-export { default as ProductSortBar } from "./ProductSortBar";
-export { default as SidebarFilter } from "./SidebarFilter";
-export { default as ProductPagination } from "./ProductPagination";
-export { default as ProductGallery } from "./ProductGallery";
-export { default as ProductInfo } from "./ProductInfo";
-export { default as ProductSpecs } from "./ProductSpecs";
-export { default as ProductHighlights } from "./ProductHighlights";
